5fc6aedf02585cc9565ea0ae2932483ee79ea5ea
nikolasmurdock <nikolasmurdock@gmail.com>
2016-09-08 13:48:14 -0500

    Logo motherfuckersssss

--- js/sphere.js
+++ js/sphere.js
@@ -1,6 +1,8 @@
 //The Scene object constructor
 var scene = new THREE.Scene();
 
+var sphere = 0;
+
 //The camera object constructor
 //It takes three arguments. 
 //fov — Camera frustum vertical field of view.
@@ -16,6 +18,9 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 //Set the background of the scene to white. 
 scene.background = new THREE.Color( 0xFFFFFF );
 
+//Set the texture
+var texture = new THREE.TextureLoader().load( 'textures/log3.png' );
+
 /****Camera Rotation Script****/
 controls = new THREE.OrbitControls( camera, renderer.domElement );
 //controls.addEventListener( 'change', render ); // add this only if there is no animation loop (requestAnimationFrame)
@@ -27,44 +32,51 @@ controls.enableZoom = false;
 //Get that renderer in the DOM
 document.body.appendChild(renderer.domElement);
 
-//LIGHTS!!!
-var ambientLight = new THREE.AmbientLight( 0x000000 );
-scene.add( ambientLight );
+var addLights = function() {
+	//LIGHTS!!!
+	var ambientLight = new THREE.AmbientLight( 0x000000 );
+	scene.add( ambientLight );
 
-var lights = [];
-lights[ 0 ] = new THREE.PointLight( 0xffffff, 1, 0 );
-lights[ 1 ] = new THREE.PointLight( 0xffffff, 1, 0 );
-lights[ 2 ] = new THREE.PointLight( 0xffffff, 1, 0 );
+	var lights = [];
+	lights[ 0 ] = new THREE.PointLight( 0xffffff, 1, 0 );
+	lights[ 1 ] = new THREE.PointLight( 0xffffff, 1, 0 );
+	lights[ 2 ] = new THREE.PointLight( 0xffffff, 1, 0 );
 
-lights[ 0 ].position.set( 0, 200, 0 );
-lights[ 1 ].position.set( 100, 200, 100 );
-lights[ 2 ].position.set( - 100, - 200, - 100 );
+	lights[ 0 ].position.set( 0, 200, 0 );
+	lights[ 1 ].position.set( 100, 200, 100 );
+	lights[ 2 ].position.set( - 100, - 200, - 100 );
 
-scene.add( lights[ 0 ] );
-scene.add( lights[ 1 ] );
-scene.add( lights[ 2 ] );
+	scene.add( lights[ 0 ] );
+	scene.add( lights[ 1 ] );
+	scene.add( lights[ 2 ] );
+}
+addLights();
 
-//Construct the sphere geometry with SphereGeometry
-//Arguments: 
-//Radius
-//widthSegments
-//HeightSegments
-var geometry = new THREE.SphereGeometry( 2, 7, 5 );
+var addSphere = function() {
+	//Construct the sphere geometry with SphereGeometry
+	//Arguments: 
+	//Radius
+	//widthSegments
+	//HeightSegments
+	var geometry = new THREE.SphereGeometry(2, 32, 32);
 
-//Add a basic material
-//var material = new THREE.MeshNormalMaterial( {color: 0x0054A6} );
-var material = new THREE.MeshPhongMaterial( {
-					color: 0x156289,
-					emissive: 0x072534,
-					side: THREE.DoubleSide,
-					shading: THREE.FlatShading
-				});
+	//Add a basic material
+	//var material = new THREE.MeshNormalMaterial( {color: 0x0054A6} );
+	var material = new THREE.MeshPhongMaterial( {
+						color: 0xFFFFFF,
+						emissive: 0x072534,
+						side: THREE.DoubleSide,
+						shading: THREE.FlatShading,
+						map: texture
+					});
 
-//Create the Mesh
-var sphere = new THREE.Mesh( geometry, material );
+	//Create the Mesh
+	sphere = new THREE.Mesh( geometry, material );
 
-//Add Sphere to Scene
-scene.add( sphere );
+	//Add Sphere to Scene
+	scene.add( sphere );
+}
+addSphere();
 
 //Move camera
 camera.position.z = 7;
@@ -83,11 +95,18 @@ function onWindowResize() {
 
 }
 
+function rotateSphere() {
+	//sphere.rotation.x += 0.01;
+	sphere.rotation.y -= 0.02;
+}
+
 function render() {
 	requestAnimationFrame(render);
 
-	sphere.rotation.x += 0.01;
-	sphere.rotation.y += 0.01;
+	/*sphere.rotation.x += 0.01;
+	sphere.rotation.y += 0.01;*/
+
+	rotateSphere();
 
 	renderer.render(scene, camera);
 	camera.lookAt( scene.position );
